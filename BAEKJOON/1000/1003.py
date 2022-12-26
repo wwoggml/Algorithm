7937a4972875c3b84b02970eb93a4bc45f02054b
wogml20 <kimjaehee18@hs.ac.kr>
2022-12-26 11:19:07 +0900

    백준 11720번 toCharArrayI() 사용(1)

--- BAEKJOON/1000/1003.py
+++
@@ -1,8 +0,0 @@
-T = int(input())
-
-for _ in range(T):
-    n = int(input())
-    zero, one = 1,0
-    for i in range(n):
-        zero, one = one, zero+one
-    print(zero, one)
---
+++ datastructure/Backjun_11720.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Backjun_11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        String sNum = br.readLine();
+        char[] cNum = sNum.toCharArray();
+
+        for(int i = 0; i<cNum.length; i++) {
+            sum += cNum[i] - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
